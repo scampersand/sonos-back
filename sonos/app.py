ff1b8454103e0b2d58f871183b25e99fc385c9a6
Aron Griffis <aron@scampersand.com>
2016-09-19 14:06:17 -0400

    add simple APIs for track info and transport state

--- sonos/app.py
+++ sonos/app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import soco
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+sonos = soco.SoCo("172.20.1.233")
 
-api.add_resource(HelloWorld, '/')
+
+class TrackInfo(Resource):
+    def get(self):
+        return sonos.get_current_track_info()
+
+api.add_resource(TrackInfo, '/track_info')
+
+
+class TransportState(Resource):
+    def get(self):
+        return {'current_transport_state':
+                sonos.get_current_transport_info()['current_transport_state']}
+
+    def put(self):
+        data = request.get_json()
+        if data['current_transport_state'] == 'PLAYING':
+            sonos.play()
+        elif data['current_transport_state'] == 'PAUSED_PLAYBACK':
+            sonos.pause()
+        elif data['current_transport_state'] == 'STOPPED':
+            sonos.stop()
+        return {'current_transport_state': data['current_transport_state']}
+
+api.add_resource(TransportState, '/transport_state')
