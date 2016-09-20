e0727a1c64c7a4a54f52e8f7b80983411f2bd6cb
Amy Griffis <amy@scampersand.com>
2016-09-20 11:59:10 -0400

    add APIs for next/back, and make more RESTful

--- sonos/app.py
+++ sonos/app.py
@@ -10,28 +10,37 @@ api = Api(app)
 sonos = soco.SoCo("172.20.1.233")
 
 
-class TrackInfo(Resource):
+class CurrentTrack(Resource):
     def get(self):
         return sonos.get_current_track_info()
 
-api.add_resource(TrackInfo, '/track_info')
-
-
-class TransportState(Resource):
-    def get(self):
-        return {'current_transport_state':
-                sonos.get_current_transport_info()['current_transport_state']}
-
-    def put(self):
+    def post(self):
         data = request.get_json()
         if not data:  # preflight request
             return ''
-        if data['current_transport_state'] == 'PLAYING':
-            sonos.play()
-        elif data['current_transport_state'] == 'PAUSED_PLAYBACK':
-            sonos.pause()
-        elif data['current_transport_state'] == 'STOPPED':
-            sonos.stop()
-        return {'current_transport_state': data['current_transport_state']}
+        if data['command'] == 'BACK':
+            sonos.previous()
+        elif data['command'] == 'NEXT':
+            sonos.next()
+        return self.get()
 
-api.add_resource(TransportState, '/transport_state')
+api.add_resource(CurrentTrack, '/current_track')
+
+
+class TransportInfo(Resource):
+    def get(self):
+        return sonos.get_current_transport_info()
+
+    def post(self):
+        data = request.get_json()
+        if not data:  # preflight request
+            return ''
+        if data['command'] == 'PLAY':
+            sonos.play()
+        elif data['command'] == 'PAUSE':
+            sonos.pause()
+        elif data['command'] == 'STOP':
+            sonos.stop()
+        return self.get()
+
+api.add_resource(TransportInfo, '/transport_info')
