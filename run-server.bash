36f175e3fed0c786f0fa1c0a0d62a0f448fae51d
Aron Griffis <aron@scampersand.com>
2016-09-27 21:04:42 -0400

    try to run flask on unique port #5

--- run-server.bash
+++ run-server.bash
@@ -2,4 +2,27 @@
 
 export FLASK_APP=sonos/app.py
 export FLASK_DEBUG=1
-flask run -h 0  # listen on all interfaces
+
+# If -h not specified, choose an IP address to listen on.
+# This could also be "0" but then doesn't display a nice clickable link.
+case " $*" in
+    *' --host'|*' -h')
+        true
+        ;;
+    *)
+        ip=$(ip a | grep -o '172[.0-9]*' | head -n1)
+        set -- "$@" -h ${ip:-0}
+        ;;
+esac
+
+# If -p not specified, choose a port automatically based on the UID.
+case " $*" in
+    *' --port'|*' -p')
+        true
+        ;;
+    *)
+        set -- "$@" -p $((5000 + EUID % 10000))
+        ;;
+esac
+
+flask run "$@"
